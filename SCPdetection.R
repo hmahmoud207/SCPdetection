d40325d2ab97de25f61577bec8ecb84660308e80
Hamdy Mahmoud <ehamdy@vt.edu>
2018-02-13 12:15:45 -0500

    fre

--- SCPdetection.R
+++ SCPdetection.R
@@ -7,3 +7,19 @@ library(SemiPar); library(KernSmooth); library(locfit); library(lokern); library
 library(pspline); library(sfsmisc)
 library(dlnm); library(locpol)
 data(chicagoNMMAPS)
+meantemp=c(); rhum=c(); death=c(); o3=c()
+
+for (i in 1:730){
+  begin=1+7*(i-1); end=7+7*(i-1)
+  meantemp[i]=mean(chicagoNMMAPS[begin:end,10]) 
+  rhum[i]=mean(chicagoNMMAPS[begin:end,12])
+  death[i]=sum(chicagoNMMAPS[begin:end,7])
+  o3[i]=mean(chicagoNMMAPS[begin:end,14])
+}
+Newdata=data.frame(meantemp,rhum,death, o3)
+Newdata=Newdata[1:574,]
+Newdata=Newdata[-446,]
+meantemp=Newdata[,1]; rhum=Newdata[,2]; death=Newdata[,3]; o3=Newdata[,4]
+plot(meantemp, death)
+plot(rhum, death)
+plot(o3, death)
