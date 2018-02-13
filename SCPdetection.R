f9c87fd3619051bba8ef64340dc6a8fc35c42989
Hamdy Mahmoud <ehamdy@vt.edu>
2018-02-13 12:14:08 -0500

    Second comm

--- SCPdetection.R
+++ SCPdetection.R
@@ -1,3 +1,9 @@
 a=1
 b=2
 c=3
+rm(list=ls())
+library(np); library(stats); library(splines); library(gam)
+library(SemiPar); library(KernSmooth); library(locfit); library(lokern); library(lpridge)
+library(pspline); library(sfsmisc)
+library(dlnm); library(locpol)
+data(chicagoNMMAPS)
